b99d5afc67cb25ce9b8b42c238bc090314f26f2a
Phillip Tribble <phillip.tribble@gmail.com>
2016-08-25 10:33:50 -0700

    Increasing timeout for java

--- jenkins/startup.sh
+++ jenkins/startup.sh
@@ -6,7 +6,7 @@ apc login --app-auth
 
 #Start Jenkins
 java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war&
-sleep 15
+sleep 30
 echo "Installing Plugins.....";echo
 java -jar /root/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin swarm github
 echo "Restarting Jenkins for plugins to work.....";echo
