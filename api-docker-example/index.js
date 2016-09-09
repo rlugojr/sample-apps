64752821cb3f9065fead667e01f32900c9e48902
Tim Statler <tim.statler@apcera.com>
2016-09-09 16:27:14 -0700

    Revert "WIP"
    This reverts commit ece7638bc88339d03e404c5a3aaea921094074ab.

--- api-docker-example/index.js
+++ api-docker-example/index.js
@@ -4,25 +4,14 @@ var WebSocket = require('ws');
 var client = new Client();
 
 // Change API endpoint to point to your cluster (e.g. api.my-cluster.com/v1/jobs/docker)
-var dockerCreateEndpoint = "http://api.cosmic.apcera-platform.io/v1/manifests"
+var dockerCreateEndpoint = "http://api.example.com/v1/jobs/docker"
 
 // Docker request object (http://docs.apcera.com/api/api-models/#createdockerjobrequest)
 // Change <USER> to your username
 var dockerRequestObject = `{
-  "jobs": {
-    "job::/sandbox/admin::my-capsule": {
-      "packages": [
-      {
-          "fqn": "package::/apcera/pkg/os::ubuntu-14.04-apc3"
-      }
-      ],
-      "ssh": true,
-      "start": {
-        "cmd": "/sbin/init"
-    },
-    "state" : "started"
-}
-}
+  "image_url":"https://registry-1.docker.io/library/nats:latest",
+  "job_fqn":"job::/sandbox/<USER>::nats",
+  "start":true
 }`;
 
 // Request arguments
@@ -53,7 +42,6 @@ function streamTaskEvents(taskLocation) {
     // Each message is a TaskEvent object (http://docs.apcera.com/api/api-models/#taskevent)
     ws.on('message', function(data, flags) {
         var taskEvent = JSON.parse(data);
-        console.log(stage, "-", subtask.name);
         var eventType = taskEvent.task_event_type;
         if (eventType == "error") {
             console.log("Task error: " + taskEvent.payload.error);
@@ -66,22 +54,7 @@ function streamTaskEvents(taskLocation) {
         var thread = taskEvent.thread;
         var stage = taskEvent.stage;
         var subtask = taskEvent.subtask;
-    });
-}
-
-function pollTaskStatus(taskURI) {
-    console.log("Starting to poll...")
-    client.get(taskURI, function(data, response) {
-        var task = data;
-        console.log(JSON.stringify(data, null, 4))
-        // console.log("Polling again in 1 sec", data);
-        // if (data.stage == "error") console.log("ERROR: " + data.payload.error);
-        // if (eventType == "error") {
-        //     console.log("Task error: " + taskEvent.payload.error);
-        //     return;
-        // }
-        // console.log(taskEvent.subtask)
-        // If task has not stopped/errored, schedule another poll
+        console.log(stage, "-", subtask.name);
     });
 }
 
@@ -89,16 +62,15 @@ function pollTaskStatus(taskURI) {
 client.post(dockerCreateEndpoint, args, function(data, response) {
     switch (response.statusCode) {
         case 200:
-        console.log("/v1/jobs/docker API request successful. " + data.location);
-            // streamTaskEvents(data.location);
-            pollTaskStatus(data.location);
+            console.log("/v1/jobs/docker API request successful.");
+            streamTaskEvents(data.location);
             break;
-            case 400:
+        case 400:
             console.log("/v1/jobs/docker API error", data.message)
             break;
-            default:
-            console.log(response.statusCode, response.statusMessage);
-        }
-    }).on('error', function(err) {
-        console.log('Error contacting API endpoint.', dockerCreateEndpoint);
-    });
+        default:
+            console.log(response.statusCode, data.message);
+    }
+}).on('error', function (err) {
+    console.log('Error contacting API endpoint.', dockerCreateEndpoint);
+});
