559815cf92d53886b8ba9e08491240d530160d25
Tim Statler <tim.statler@apcera.com>
2016-09-09 16:33:32 -0700

    Removed rogue files

--- example-bash-apptoken/bash_setup.sh
+++
@@ -1,7 +0,0 @@
-# Set the target cluster (e.g. your-cluster.apcera-platform.io):
-export target=cluster-name.example.com
-
-# Download APC from target cluster and unzip (note: this endpoint does not require authentication)
-wget http://api.$target/v1/apc/download/linux_amd64/apc.zip
-unzip apc.zip
-rm apc.zip
