63f6572920b56d09e54d3d1bf4e57440290427a7
Phillip Tribble <phillip.tribble@gmail.com>
2016-09-15 08:52:13 -0700

    Adding build pipeline

--- jenkins/startup.sh
+++ jenkins/startup.sh
@@ -8,7 +8,7 @@ apc login --app-auth
 java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war&
 sleep 30
 echo "Installing Plugins.....";echo
-java -jar /root/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin swarm github
+java -jar /root/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin swarm github pipeline workflow-job workflow-basic-steps
 echo "Restarting Jenkins for plugins to work.....";echo
 java -jar /root/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ restart
 echo "Sleeping forever.....";echo
