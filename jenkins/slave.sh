afaf3dad23c7a790b39dd5911d7a24080fcb55b7
Juan Tellez <juan@apcera.com>
2016-12-30 20:53:58 -0800

    add apc back in

--- jenkins/slave.sh
+++ jenkins/slave.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# Initializing Apcera
+# Apcera apc CLI
+route=`echo $TARGET | cut -d '/' -f3`
+wget https://api.${route}/v1/apc/download/linux_amd64/apc.zip
+unzip apc.zip -d /usr/local/bin
+apc target $TARGET
+apc login --app-auth
+
 # start the slave
 java -jar cli.jar -fsroot /root/.jenkins/workspace -master http://master.apcera.local:8080 -executors 1
